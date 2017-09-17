c98d6071b5453810f4ffc6f1d745c2e18c04db16
nrliangxy <nrliangxy@foxmail.com>
2017-09-17 19:41:11 +0800

    update sklearn_method

--- industry/sklearn_method.py
+++ industry/sklearn_method.py
@@ -1,38 +1,35 @@
 import pandas as pd
-import pickle
-import nltk
-def getTrainData(path,num):
-	df=pd.read_csv(path)
-	trainingset=[]
-	for line in df['des']:
-		if num//10 == 0:labl="0Agriculture, Forestry & Fishing"
-		elif 15>num>9: labl="1Mining"
-		elif 18>num>14: labl="2Construction"
-		elif 40>num>19: labl="3Manufacturing"
-		elif num//10 == 4: labl="4Transportation, Communications, Electric, Gas & Sanitary Services"
-		elif 52>num>49 : labl="5Wholesale Trade"
-		elif 60>num>51: labl="6Retail Trade"
-		elif 68>num>59: labl="7Finance, Insurance & Real Estate"
-		elif 90>num>69: labl="8services"
-		elif 99>num>90: labl="9Public Administration"
-		trainingset.append((line,labl))
-	return trainingset
-train_set=[]
-test_set=[]
-for i in range(100):
-	try:
-		train_test_all=getTrainData("/home/lxy/Downloads/category/category%.2d.csv"%i,i)
-		if len(train_test_all)>10:
-			train_set.extend(train_test_all[:-5])
-			test_set.extend(train_test_all[-5:])
-			print (i,len(train_set),len(test_set))
-	except:continue
-train_data_list, train_class_list = zip(*train_set)
-test_date_list, test_class_list = zip(*test_set)
-from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 import re
 import pickle
+def getTrainData(path,num):
+    df=pd.read_csv(path)
+    trainingset=[]
+    for line in df['des']:
+        if num//10 == 0:labl="0Agriculture, Forestry & Fishing"
+        elif 15>num>9: labl="1Mining"
+        elif 18>num>14: labl="2Construction"
+        elif 40>num>19: labl="3Manufacturing"
+        elif num//10 == 4: labl="4Transportation, Communications, Electric, Gas & Sanitary Services"
+        elif 52>num>49 : labl="5Wholesale Trade"
+        elif 60>num>51: labl="6Retail Trade"
+        elif 68>num>59: labl="7Finance, Insurance & Real Estate"
+        elif 90>num>69: labl="8services"
+        elif 99>num>90: labl="9Public Administration"
+        trainingset.append((line,labl))
+    return trainingset
+train_set=[]
+test_set=[]
+for i in range(100):
+    try:
+        train_test_all=getTrainData("/home/lxy/Downloads/category/category%.2d.csv"%i,i)
+        if len(train_test_all)>10:
+            train_set.extend(train_test_all[:-5])
+            test_set.extend(train_test_all[-5:])
+            print (i,len(train_set),len(test_set))
+    except:continue
+train_data_list, train_class_list = zip(*train_set)
+test_date_list, test_class_list = zip(*test_set)
 def remove_noise(document):
     noise_pattern = re.compile("|".join(["http\S+","\@\w+","\#\w+"]))
     clean_text = re.sub(noise_pattern,"",document)
